3f092615a5fe5aebcc4452fe68fc1c401a8123fc
Alikhani <alikhani.dev@gmail.com>
2022-01-17 14:25:16 +0330

    add get Weather with IP address & Update README

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
-## Preview
+# Weather App
+
+A simple weather app built with React
+
+## Preview Desktop
 
 ![Preview](https://user-images.githubusercontent.com/87765316/149657273-45565759-dbb1-48fd-b1a7-c8f0e6733ade.gif)
+
+## Preview Mobile
+
+![doc_2022-01-17_14-14-37](https://user-images.githubusercontent.com/87765316/149756007-f239d6c4-3e34-42df-91ea-3ab2e633d9f4.gif)
 
 ## Features
 
 - [x] Responsive
 - [x] Search city
 - [x] Get weather by location ( GPS )
+- [x] Get weather by Address ( IP )
 
 ## Tech Stack
 
-**Client:** React
+**Client:** React , React-Toastify
+**APi:** OpenWeather , Snoopi
 
 ## Contact US
 
--- src/hook/useForecast.js
+++ src/hook/useForecast.js
@@ -8,6 +8,19 @@ const useForecast = () => {
 	const showSearch = () => setForecast(null)
 	const removeError = () => setError(false)
 
+	const geolocationByIP = async () => {
+		setLoading(true)
+		setError(false)
+		const { Latitude, Longitude } = await fetch(`${process.env.REACT_APP_IP_ADDRESS}?apikey=${process.env.REACT_APP_IP_ADDRESS_KEY}`)
+			.then((res) => res.json())
+			.finally(() => setLoading(false))
+		if (Latitude && Longitude) {
+			findByGPS(Latitude, Longitude)
+		} else {
+			setError('Error server')
+		}
+	}
+
 	const findCity = async (location) => {
 		setLoading(true)
 		setError(false)
@@ -35,7 +48,7 @@ const useForecast = () => {
 
 	const getLocation = async () => {
 		if ('geolocation' in navigator) {
-			await navigator.geolocation.getCurrentPosition(success, error, { enableHighAccuracy: true })
+			await navigator.geolocation.getCurrentPosition(success, geolocationByIP, { enableHighAccuracy: true })
 		} else {
 			setError('browser not supported')
 		}
@@ -45,27 +58,6 @@ const useForecast = () => {
 		const { latitude, longitude } = coords
 		showSearch()
 		findByGPS(latitude, longitude)
-	}
-
-	const error = ({ code }) => {
-		switch (code) {
-			case 1:
-				setError(
-					`The acquisition of the geolocation information failed because the page didn't have the permission to do it.`,
-				)
-				break
-			case 2:
-				setError(
-					`The acquisition of the geolocation failed because one or several internal sources of position returned an internal error.`,
-				)
-				break
-			case 3:
-				setError(`Geolocation information was not obtained in the allowed time.`)
-				break
-			default:
-				setError(`Please see later`)
-				break
-		}
 	}
 
 	return {
